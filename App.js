d33399fd886e48f6aeaeb7f82a83c4141bcbce0d
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2021-03-14 13:06:10 +0000

    src folder started

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+
+//TODO: Install react-navigation and import all the screens here for it to use
+//"The app.js file is usually used for the initial screen you want to display on start-up of the app. But also most people make this app.js file into a navigator file where you can import all the navigation screens."
+//(reference: https://stackoverflow.com/questions/62176307/how-do-you-structure-components-and-screens-when-using-expo-native-base-and-rea)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Test</Text>
+      <StatusBar />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
